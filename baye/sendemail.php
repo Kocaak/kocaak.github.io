6250139841ed3911c0a951bf5f6e5cefdcd918d5
Murat Kocak <murathkocak@gmail.com>
2018-11-16 23:57:52 +0300

    v4

--- baye/sendemail.php
+++ baye/sendemail.php
@@ -2,18 +2,34 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+		'message'=>'Basvuru icin tesekkurler. En kisa surede donus saglayacagiz. '
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['ad_soyad']));
+    $email = @trim(stripslashes($_POST['email']));
+    $telefon = @trim(stripslashes($_POST['telefon']));
+    $adres = @trim(stripslashes($_POST['adres']));
+		$dogum = @trim(stripslashes($_POST['dogum']));
+		$medeni = @trim(stripslashes($_POST['medeni']));
+		$askerlik = @trim(stripslashes($_POST['askerlik']));
+		$surucu = @trim(stripslashes($_POST['surucu']));
+		$lise = @trim(stripslashes($_POST['lise']));
+		$uni = @trim(stripslashes($_POST['uni']));
+		$sabika = @trim(stripslashes($_POST['sabika']));
+		$saglik = @trim(stripslashes($_POST['saglik']));
+		$web = @trim(stripslashes($_POST['web']));
+		$yabancidil = @trim(stripslashes($_POST['yabancidil']));
+		$progdil = @trim(stripslashes($_POST['progdil']));
+		$tecrube = @trim(stripslashes($_POST['tecrube']));
+		$kurs = @trim(stripslashes($_POST['kurs']));
+		$referans = @trim(stripslashes($_POST['referans']));
+		$mesaj = @trim(stripslashes($_POST['mesaj']));
+		$subject = 'Yeni Is Basvurusu';
 
     $email_from = $email;
-    $email_to = 'email@email.com';//replace with your email
+    $email_to = 'murathkocak@gmail.com';
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $mesaj;
 
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
